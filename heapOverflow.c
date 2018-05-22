ccc6f8b473d3b8eb674deabad59bc3dee11224a4
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2018-05-22 08:17:06 -0300

    Create heapOverflow.c

---
+++ heapOverflow.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+void main(void){
+    char buff[15];
+    
+    printf("\n Digite a senha: \n");
+    
+    gets(buff);
+    
+    if(strcmp(buff, "segredo") == 0 ){
+        printf ("\n Senha correta! \n");
+    }else{
+        printf ("\n Senha incorreta! \n");
+    }
+}
